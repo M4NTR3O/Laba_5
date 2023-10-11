29be66775119ff1bb5419e6cf6273dd8ed6f13c2
emil_ <emil.kushner.stud@mail.ru>
2023-10-11 10:23:25 +0700

    Дизайн главной активити и инициализация элементов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var okButton: Button
+    private lateinit var dollarButton: RadioButton
+    private lateinit var euroButton: RadioButton
+    private lateinit var poundButton: RadioButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        okButton = findViewById(R.id.buttonOk)
+        dollarButton = findViewById(R.id.radioDollar)
+        euroButton = findViewById(R.id.radioEuro)
+        poundButton = findViewById(R.id.radioPound)
+        okButton.setOnClickListener {
+
+        }
     }
 }
