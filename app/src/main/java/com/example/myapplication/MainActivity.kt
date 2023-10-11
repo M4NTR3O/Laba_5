92f1fc0f4d7277c08ef3718d60d8109e0964ba01
emil_ <emil.kushner.stud@mail.ru>
2023-10-11 19:39:56 +0700

    Дизайн 2 активити, инициализация элементов активити и попытка перехода с данными на вторую активити

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var okButton: Button
     private lateinit var dollarButton: RadioButton
     private lateinit var euroButton: RadioButton
     private lateinit var poundButton: RadioButton
+    private lateinit var costText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,8 +21,13 @@ class MainActivity : AppCompatActivity() {
         dollarButton = findViewById(R.id.radioDollar)
         euroButton = findViewById(R.id.radioEuro)
         poundButton = findViewById(R.id.radioPound)
+        costText = findViewById(R.id.costText)
         okButton.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("input_cost", costText.text)
+            intent.putExtra("dollar", dollarButton.isChecked)
+            intent.putExtra("euro", euroButton.isChecked)
+            intent.putExtra("pound", poundButton.isChecked)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/myapplication/ResultActivity.kt
+++ app/src/main/java/com/example/myapplication/ResultActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var textInput: TextView
+    private lateinit var textOutput: TextView
+    private lateinit var textValue: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 }
